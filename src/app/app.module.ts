7162f1581c94d26738f6465928a77a5d394a7719
shajn786 <shajn786@gmail.com>
2022-11-29 00:52:46 +0530

    viewall

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddNewComponent } from './add-new/add-new.component';
+import { NewViewAllComponent } from './new-view-all/new-view-all.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddNewComponent
+    AddNewComponent,
+    NewViewAllComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/new-view-all/new-view-all.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-view-all',
+  templateUrl: './new-view-all.component.html',
+  styleUrls: ['./new-view-all.component.css']
+})
+export class NewViewAllComponent {
+
+}
